fac78c72172c9c79fc96412fb8ce64123d91b583
Ivan Perez <ivan.perezdominguez@nasa.gov>
2024-01-01 21:42:34 -0800

    Remove URL

--- copilotlanguage.rb
+++ copilotlanguage.rb
@@ -1,8 +1,6 @@
 class Copilotlanguage < Formula
   desc "Stream-based Programming Language for real time"
   homepage "copilot-language.github.io"
-  url "https://github.com/Copilot-Language/copilot/archive/refs/tags/v3.17.tar.gz"
-  sha256 "a567c9b8c094bec85238cae5c305c77fef1444359e793a54c21c5f15e35b5515"
   license "BSD-3-Clause"
   head "https://github.com/copilot-language/copilot.git", branch: "master"
 
@@ -10,6 +8,6 @@ class Copilotlanguage < Formula
 
   def install
     system "cabal", "v2-update"
-    system "cabal", "v2-install", *std_cabal_v2_args, "."
+    system "cabal", "v2-install", *std_cabal_v2_args, "copilot-3.17"
   end
 end
