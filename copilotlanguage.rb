6d863223cafcfac7e9afaa30ec9540f0e93b5cc5
Ivan Perez <ivan.perezdominguez@nasa.gov>
2024-01-01 21:24:21 -0800

    All subdirectories containing a cabal file

--- copilotlanguage.rb
+++ copilotlanguage.rb
@@ -10,6 +10,6 @@ class Copilotlanguage < Formula
 
   def install
     system "cabal", "v2-update"
-    system "cabal", "v2-install", *std_cabal_v2_args
+    system "cabal", "v2-install", *std_cabal_v2_args, "*/*.cabal"
   end
 end
