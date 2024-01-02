91e4ba096d94e7828bddb2cb99b43e03f21d7edb
Ivan Perez <ivan.perezdominguez@nasa.gov>
2024-01-01 21:54:03 -0800

    Bring URL back

--- copilotlanguage.rb
+++ copilotlanguage.rb
@@ -1,6 +1,8 @@
 class Copilotlanguage < Formula
   desc "Stream-based Programming Language for real time"
   homepage "copilot-language.github.io"
+  url "https://github.com/Copilot-Language/copilot/archive/refs/tags/v3.17.tar.gz"
+  sha256 "a567c9b8c094bec85238cae5c305c77fef1444359e793a54c21c5f15e35b5515"
   license "BSD-3-Clause"
   head "https://github.com/copilot-language/copilot.git", branch: "master"
 
